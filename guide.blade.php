3927deff5a8699bff7b6a1cd03d001f6bc6a7073
larigyn <androidagay@gmail.com>
2017-10-09 02:03:33 +0800

    initial commit

---
+++ guide.blade.php
@@ -0,0 +1,17 @@
+{{-- git config --}}
+git config --global user.name "larigyn"
+git config --global user.email androidagay@gmail.com
+
+{{-- git list --}}
+git config --list
+
+{{-- git initialized --}}
+git init
+
+{{-- git status --}}
+git status
+
+git add (filename)
+git add . (all files)
+
+
